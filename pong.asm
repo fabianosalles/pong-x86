7d5dc74506ce62bea011344a83449be30638b720
Fabiano Xavier <fabiano.xavier@acrelec.com>
2020-08-03 00:17:14 -0300

    basic game loop

--- pong.asm
+++ pong.asm
@@ -8,39 +8,26 @@
 ; Use Up and down keys to move player 2.
 ;
 
+format binary       		; flat binary file format
 use16 						; 16bit bin (dos COM)
-org	0x0100
+org	0x0100	
+jmp start
 
-; -------------------------------------------------------
-;  DEFINES AND CONSTANTS
-; -------------------------------------------------------
-	VGA_BASE				= 0xA000
-	VGA_SEGMENT				equ gs
-	VGA_WORDS_COUNT			= 0x7D00  ; number of words in display memory (320 * 200 / 2) 	
-	MAXX					= 320-1
-	MAXY					= 200-1
-	
-	VK_SCAPE				= 01h
-	VK_W                 	= 11h
-	VK_S                 	= 1Fh
-	VK_UP					= 48h	
-	VK_DOWN					= 50h	
-	VK_LEFT					= 4Bh
-	VK_RIGHT				= 4Dh
-	
-	jmp start
+include 'inc/vga13h.inc'
 
-; -------------------------------------------------------
-;  CODE
-; -------------------------------------------------------
 start:
-   cli 							; disable interrupts 
-   mov	ax, cs           		; code,data,stack share same segment 
+   cli 						; disable interrupts 
+   mov	ax, cs           	; code,data,stack share same segment 
    mov	ds, ax 
    mov	ss, ax 
    xor	sp, sp 
- 
-   mov	ax, VGA_BASE      		; Video memory segment 
-   mov	VGA_SEGMENT, ax 		; will be in gs 
-   sti							; we'e done so, reenable interrupts
+   cld
+   mov	ax, VGA_BASE      	; Video memory segment 
+   mov	VGA_SEGMENT, ax 	; will be in gs 
+   sti						; we'e done so, reenable interrupts
+   call init_graphics       ; init VGA mode 13h 
+main:
+	clear_screen COLOR_DGRAY
 
+	call swap_buffers
+	jmp main
