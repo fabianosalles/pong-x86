3d4df86b394cd1dc493c1131461ac127cdf41858
Fabiano Xavier <fabiano.xavier@acrelec.com>
2020-08-06 19:34:56 -0300

    game drawing stage basics

--- pong.asm
+++ pong.asm
@@ -14,20 +14,29 @@ org	0x0100
 jmp start
 
 include 'inc/vga13h.inc'
+include 'game.inc'
 
 start:
-   cli 						; disable interrupts 
-   mov	ax, cs           	; code,data,stack share same segment 
-   mov	ds, ax 
-   mov	ss, ax 
-   xor	sp, sp 
-   cld
-   mov	ax, VGA_BASE      	; Video memory segment 
-   mov	VGA_SEGMENT, ax 	; will be in gs 
-   sti						; we'e done so, reenable interrupts
-   call init_graphics       ; init VGA mode 13h 
+	cli 					; disable interrupts 
+	mov	ax, cs           	; code,data,stack share same segment 
+	mov	ds, ax 
+	mov	ss, ax 
+	xor	sp, sp 
+	cld                     ; clear direction flag for string instructions
+	mov	ax, VGA_BASE      	; Video memory segment 
+	mov	VGA_SEGMENT, ax 	; will be in gs 
+	sti						; we'e done so, reenable interrupts
 main:
-	clear_screen COLOR_DGRAY
+	call init_graphics      			; init VGA mode 13h
+	; game main loop 
+	.loop:								
+		clear_screen COLOR_BLACK
+        
+		; todo
+		; 1. capture input
+		; 2. update state
+		; 3. draw game
+		call draw_game
 
-	call swap_buffers
-	jmp main
+		call swap_buffers
+		jmp main.loop
